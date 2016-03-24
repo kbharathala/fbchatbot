397cff49c1fafbc03b166194be849fbd43f7f616
kbharathala <kbharathala@gmail.com>
2016-03-24 15:40:12 -0400

    Hiding Passwords

--- commands/yelp.js
+++ commands/yelp.js
@@ -20,25 +20,25 @@ module.exports.handler = (api, args, message) => {
   if(args.length < 1) {
   	api.sendMessage("What's your zipcode?", message.threadID);
   } else if(args.length == 1) {
-  	yelp.search({ term: 'food', location: args[0] })
-	.then(function (data) {
-    api.sendMessage(data.businesses[0].name, message.threadID);
-	})
-	.catch(function (err) {
-	  api.sendMessage("Sorry. There are no results for that entry", message.threadID);
-	});
+    	yelp.search({ term: 'food', location: args[0] })
+  	.then(function (data) {
+      api.sendMessage(data.businesses[0].name, message.threadID);
+  	})
+  	.catch(function (err) {
+  	  api.sendMessage("Sorry. There are no results for that entry", message.threadID);
+  	});
   } else {
   	let zip = args[0];
   	args.shift()
   	let cuisine = args.join(' ');
   	yelp.search({ term: cuisine, location: zip})
-	.then(function (data) {
-    api.sendMessage(data.businesses[0].name, message.threadID);
-	})
-	.catch(function (err) {
-	  api.sendMessage("Sorry. There are no results for that entry", message.threadID);
-	});
-}
+  	.then(function (data) {
+      api.sendMessage(data.businesses[0].name, message.threadID);
+  	})
+  	.catch(function (err) {
+  	  api.sendMessage("Sorry. There are no results for that entry", message.threadID);
+  	});
+  }
 };
 
 
